5a96cbe032ef3500554950e7b430e4871aab7d81
hopkins385 <stadhouders.sven@gmail.com>
2025-03-08 16:51:53 +0100

    comments

--- src/components/editor/EditorComments.vue
+++ src/components/editor/EditorComments.vue
@@ -29,7 +29,10 @@ const editor = editorStore.getEditor();
 
 <template>
   <transition name="slide">
-    <div v-if="editorStore.showComments" class="comments-panel h-full overflow-hidden shrink-0">
+    <div
+      v-if="editorStore.showComments"
+      class="comments-panel h-full overflow-x-hidden overflow-y-scroll shrink-0"
+    >
       <div class="border-l h-full p-4 space-y-5">
         <div class="flex items-center space-x-1">
           <div>
@@ -44,15 +47,17 @@ const editor = editorStore.getEditor();
         <div>
           <CommentAddForm @submit="editorStore.addComment" />
         </div>
-        <div v-for="comment in editorStore.comments" :key="comment.id" class="comment">
-          <div
-            class="border p-2 text-sm rounded-md"
-            :class="{
-              'bg-gray-100': comment.id === editorStore.selectedCommentId,
-            }"
-          >
-            <p>{{ comment.text }}</p>
-            <button @click="() => editorStore.deleteComment(comment.id)">Delete</button>
+        <div class="pb-5 space-y-5">
+          <div v-for="comment in editorStore.comments" :key="comment.id">
+            <div
+              class="border p-2 text-sm rounded-md"
+              :class="{
+                'bg-gray-100': comment.id === editorStore.selectedCommentId,
+              }"
+            >
+              <p>{{ comment.text }}</p>
+              <button @click="() => editorStore.deleteComment(comment.id)">Delete</button>
+            </div>
           </div>
         </div>
       </div>
--- src/views/document/DocumentIndexView.vue
+++ src/views/document/DocumentIndexView.vue
@@ -2,9 +2,10 @@
 import EditorComments from '@/components/editor/EditorComments.vue';
 import EditorContainer from '@/components/editor/EditorContainer.vue';
 import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
 import { useDrawerStore } from '@/stores/drawer.store';
 import { useEditorStore } from '@/stores/editor.store';
-import { MessageCircle } from 'lucide-vue-next';
+import { BookmarkIcon, BotMessageSquare, MessageCircle } from 'lucide-vue-next';
 
 const drawer = useDrawerStore();
 const editorStore = useEditorStore();
@@ -22,10 +23,19 @@ onMounted(() => {
 
     <EditorComments />
 
-    <div class="w-16 z-10 border-l h-full p-2 shrink-0 bg-white">
-      <Button variant="ghost" size="icon" @click="() => editorStore.toggleShowComments()">
-        <MessageCircle class="size-5" />
-      </Button>
+    <div class="w-14 z-10 border-l h-full p-2 shrink-0 bg-white">
+      <div class="flex flex-col items-center h-full space-y-1 pt-2">
+        <Button variant="ghost" size="icon">
+          <BotMessageSquare class="size-5" />
+        </Button>
+        <Separator class="!my-1" />
+        <Button variant="ghost" size="icon" @click="() => editorStore.toggleShowComments()">
+          <MessageCircle class="size-5" />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <BookmarkIcon class="size-5" />
+        </Button>
+      </div>
     </div>
     <!-- mt-[7.5rem] -->
   </div>
